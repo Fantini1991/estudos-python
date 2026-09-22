cf01b10c87b5d9098d9bfbce2b90388e91c6bd8a
Fantini1991 <guilhermelogli@hotmail.com>
2026-09-22 13:31:51 -0300

    Aula 7: introducao a SQL com SELECT, WHERE e GROUP BY

---
+++ aula7_sql.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pandas as pd
+
+tabela = pd.read_csv("medicoes.csv")
+
+conexao = sqlite3.connect("lab.db")
+tabela.to_sql("medicoes", conexao, if_exists="replace", index=False)
+
+resultado_1 = pd.read_sql("SELECT amostra, ph FROM medicoes WHERE ph < 6.5 OR ph > 7.5 ", conexao)
+
+resultado_2 = pd.read_sql("""
+SELECT lote, COUNT(*), AVG(ph)
+FROM medicoes
+GROUP BY lote
+""", conexao)
+
+print(resultado_1)
+print()
+print(resultado_2)
