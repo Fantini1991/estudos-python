e4506575d7523c1c8cdfd384f9286f2333c84e76
Fantini1991 <guilhermelogli@hotmail.com>
2026-09-22 10:42:40 -0300

    Aula 6: resumo estatistico e contagem de FORA por lote

--- aula6b.py
+++ aula6b.py
@@ -6,5 +6,11 @@ tabela["temperatura_k"] = tabela["temperatura"] + 273.15
 tabela["status"] = "OK"
 
 tabela.loc[ (tabela ["ph"] < 6.5) | (tabela ["ph"] > 7.5), "status" ] = "FORA"
+resumo_lote = tabela.groupby("lote")["ph"].agg(["count", "mean", "median", "min", "max"])
+lotes_fora = tabela.groupby("lote")["status"].value_counts()
 
 print(tabela)
+print()
+print(resumo_lote)
+print()
+print(lotes_fora)
