bd16a112eab01aa36466c71616b5b9fd4a379957
Fantini1991 <guilhermelogli@hotmail.com>
2026-09-22 09:45:32 -0300

    Aula 6b: coluna status calculada com loc

--- aula6b.py
+++ aula6b.py
@@ -3,5 +3,8 @@ import pandas as pd
 tabela = pd.read_csv("medicoes.csv")
 
 tabela["temperatura_k"] = tabela["temperatura"] + 273.15
+tabela["status"] = "OK"
+
+tabela.loc[ (tabela ["ph"] < 6.5) | (tabela ["ph"] > 7.5), "status" ] = "FORA"
 
 print(tabela)
